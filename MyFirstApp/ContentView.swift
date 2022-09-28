b7e1f2056c7e2f4695b1667a1bd409c16f93f58d
GNYAR <s901121s901121@gmail.com>
2022-09-28 15:13:38 +0800

    feat(toolbar): implement the layout

--- MyFirstApp/ContentView.swift
+++ MyFirstApp/ContentView.swift
@@ -8,14 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  var body: some View {
+    ZStack {
+      Color(.white).ignoresSafeArea() // Background
+      
+      ScrollView {
+        VStack {
+          HStack { // Toolbar
+            Image("FullMenuLogo")
+              .resizable()
+              .frame(width: 48, height: 48)
+            
+            Spacer()
+            
+            Image("card-heart")
+              .renderingMode(.template)
+              .resizable()
+              .frame(width: 28, height: 28)
+              .rotationEffect(.degrees(60))
+              .foregroundColor(.red)
+            
+            ZStack {
+              Image("bell-outline")
+                .renderingMode(.template)
+                .resizable()
+                .frame(width: 28, height: 28)
+                .foregroundColor(.gray)
+              
+              Text("2")
+                .font(Font.system(size: 12))
+                .foregroundColor(.white)
+                .frame(width: 14, height: 14)
+                .background(Color.yellow)
+                .clipShape(Circle())
+                .offset(x: 5, y: -5)
+            }
+          }
+          .frame(
+            minWidth: 0,
+            maxWidth: .infinity,
+            minHeight: 0,
+            maxHeight: .infinity,
+            alignment: .topLeading
+          )
+          
+        }
+        .frame(alignment: .top)
+        .padding(.horizontal, 8)
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
